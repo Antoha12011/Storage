06fb8fd0ce2e244eb26f617b4fe98a69dc0e8717
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-01-19 20:17:33 +0300

    Initial Commit

---
+++ Storage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storage
+//
+//  Created by Антон Павлов on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
